f05c3d9c4bf9d5983999ed22261da712d1eb0717
IBEABUCHI MIRACLE <chikimiracle2000@gmail.com>
2025-04-24 09:45:01 +0100

    chore: add correct dependency for clerk/tanstack-start
    feat: add functionality for authentication using clerk auth

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,30 @@
-import { createRouter as createTanStackRouter } from '@tanstack/react-router'
+import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import {
   MutationCache,
   QueryClient,
   notifyManager,
-} from '@tanstack/react-query'
-import { routerWithQueryClient } from '@tanstack/react-router-with-query'
-import toast from 'react-hot-toast'
-import { ConvexQueryClient } from '@convex-dev/react-query'
-import { ConvexProvider } from 'convex/react'
-import { routeTree } from './routeTree.gen'
-import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
-import { NotFound } from './components/NotFound'
+} from "@tanstack/react-query";
+import { routerWithQueryClient } from "@tanstack/react-router-with-query";
+import toast from "react-hot-toast";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { routeTree } from "./routeTree.gen";
+import { DefaultCatchBoundary } from "./components/DefaultCatchBoundary";
+import { NotFound } from "./components/NotFound";
 
 export function createRouter() {
-  if (typeof document !== 'undefined') {
-    notifyManager.setScheduler(window.requestAnimationFrame)
+  if (typeof document !== "undefined") {
+    notifyManager.setScheduler(window.requestAnimationFrame);
   }
 
-  const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!
+  const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!;
   if (!CONVEX_URL) {
-    console.error('missing envar CONVEX_URL')
+    console.error("missing envar CONVEX_URL");
   }
-  const convexQueryClient = new ConvexQueryClient(CONVEX_URL)
+  const convex = new ConvexReactClient(CONVEX_URL, {
+    unsavedChangesWarning: false,
+  });
+  const convexQueryClient = new ConvexQueryClient(convex);
 
   const queryClient: QueryClient = new QueryClient({
     defaultOptions: {
@@ -32,19 +35,19 @@ export function createRouter() {
     },
     mutationCache: new MutationCache({
       onError: (error) => {
-        toast(error.message, { className: 'bg-red-500 text-white' })
+        toast(error.message, { className: "bg-red-500 text-white" });
       },
     }),
-  })
-  convexQueryClient.connect(queryClient)
+  });
+  convexQueryClient.connect(queryClient);
 
   const router = routerWithQueryClient(
     createTanStackRouter({
       routeTree,
-      defaultPreload: 'intent',
+      defaultPreload: "intent",
       defaultErrorComponent: DefaultCatchBoundary,
       defaultNotFoundComponent: () => <NotFound />,
-      context: { queryClient },
+      context: { queryClient, convexClient: convex, convexQueryClient },
       Wrap: ({ children }) => (
         <ConvexProvider client={convexQueryClient.convexClient}>
           {children}
@@ -52,14 +55,14 @@ export function createRouter() {
       ),
       scrollRestoration: true,
     }),
-    queryClient,
-  )
+    queryClient
+  );
 
-  return router
+  return router;
 }
 
-declare module '@tanstack/react-router' {
+declare module "@tanstack/react-router" {
   interface Register {
-    router: ReturnType<typeof createRouter>
+    router: ReturnType<typeof createRouter>;
   }
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,6 @@
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
+import { ClerkProvider, useAuth } from "@clerk/tanstack-react-start";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 import {
   Link,
   Outlet,
@@ -6,20 +8,41 @@ import {
   useRouterState,
   HeadContent,
   Scripts,
+  useRouteContext,
 } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import * as React from "react";
 import { Toaster } from "react-hot-toast";
 import type { QueryClient } from "@tanstack/react-query";
 import { NotFound } from "@/components/NotFound";
-// import appCss from "@/styles/app.css?url";
 import rootCss from "@/styles/globals.css?url";
 import { seo } from "@/utils/seo";
 import { DefaultCatchBoundary } from "@/components/DefaultCatchBoundary";
 import { Loader } from "@/components/Loader";
+import { ConvexReactClient } from "convex/react";
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import { createServerFn } from "@tanstack/react-start";
+import { getAuth } from "@clerk/tanstack-react-start/server";
+import { getWebRequest } from "@tanstack/react-start/server";
+
+const fetchClerkAuth = createServerFn({ method: "GET" }).handler(async () => {
+  const request = getWebRequest();
+  if (!request) return;
+  const auth = await getAuth(request, {
+    secretKey: process.env.CLERK_SECRET_KEY,
+  });
+  const token = await auth.getToken({ template: "convex" });
+
+  return {
+    userId: auth.userId,
+    token,
+  };
+});
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
+  convexClient: ConvexReactClient;
+  convexQueryClient: ConvexQueryClient;
 }>()({
   head: () => ({
     meta: [
@@ -62,6 +85,22 @@ export const Route = createRootRouteWithContext<{
       { rel: "icon", href: "/favicon.ico" },
     ],
   }),
+  beforeLoad: async (ctx) => {
+    const auth = await fetchClerkAuth();
+    if (!auth) return;
+    const { userId, token } = auth;
+
+    // During SSR only (the only time serverHttpClient exists),
+    // set the Clerk auth token to make HTTP queries with.
+    if (token) {
+      ctx.context.convexQueryClient.serverHttpClient?.setAuth(token);
+    }
+
+    return {
+      userId,
+      token,
+    };
+  },
   errorComponent: (props) => {
     return (
       <RootDocument>
@@ -82,19 +121,24 @@ function RootComponent() {
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
+  const context = useRouteContext({ from: Route.id });
   return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Toaster />
-        <ReactQueryDevtools />
-        <TanStackRouterDevtools position="bottom-left" />
-        <Scripts />
-      </body>
-    </html>
+    <ClerkProvider>
+      <ConvexProviderWithClerk client={context.convexClient} useAuth={useAuth}>
+        <html>
+          <head>
+            <HeadContent />
+          </head>
+          <body>
+            {children}
+            <Toaster />
+            <ReactQueryDevtools />
+            <TanStackRouterDevtools position="bottom-left" />
+            <Scripts />
+          </body>
+        </html>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 }
 
